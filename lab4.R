bf8f38f94518597d4aad0f0682b520204e4e95c2
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-07 16:08:15 +0530

    Box-Plot, Histogram and Stem-Leaf Plot

--- lab4.R
+++ lab4.R
@@ -15,3 +15,21 @@ data
 #accessing variables or columns directly without calling whole dataset
 attach(data)
 
+#boxplot
+boxplot(Attendance,main="Boxplot for attendance",outline = TRUE, xlab="Attendance", horizontal = TRUE, outpch=8)
+boxplot(Salary, main="Boxplot for salary", outline=TRUE,xlab="Salary", horizontal = TRUE, outpch=8)
+boxplot(Years, main="Boxplot for years", outline = TRUE, xlab="Years", horizontal = TRUE, outpch=8)
+
+
+#histogram
+hist(Attendance, main = "histogram for attendance", ylab="Frequency")
+abline(h=0)
+hist(Salary, main = "histogram for Salary", ylab="Frequency")
+abline(h=0)
+hist(Years, main = "histogram for Years", ylab="Frequency")
+abline(h=0)
+
+#stem-leaf
+stem(Attendance)
+stem(Salary)
+stem(Years)
